1006a071d02147cc46b7222763d239df900a67a5
lchen104 <lchen@zdmedia.com>
2023-07-24 16:07:21 -0400

    Steps app - first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+  // const step = 1;
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  // const handlePrevious = () => {
+  //   if (step > 1) {
+  //   // console.log(step)
+  //     setStep(step - 1)
+  //   }
+  // }
+  const handlePrevious = () => step > 1 && setStep(step - 1);
+
+  // const handleNext = () => {
+  //   if (step < 3) {
+  //   // console.log(step)
+  //     setStep(step + 1)
+  //   }
+  // }
+  const handleNext = () => step < 3 && setStep(step + 1);
+
+
+  // const handleClose = () => setIsOpen(false);
+
+  // const handleOpen = () => setIsOpen(true);
+
+
+  const handleClopen = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* if isOpen is true, render component, else '' */}
+
+      {isOpen ? (
+        <div>
+          <button onClick={handleClopen} className='close'> &times; </button>
+          <div className="steps">
+            <div className="numbers">
+              <div className={step >= 1 ? "active" : ""}>1</div>
+              <div className={step >= 2 ? "active" : ""}>2</div>
+              <div className={step >= 3 ? "active" : ""}>3</div>
+            </div>
+            <p className="message">
+              {" "}
+              Step {step} : {messages[step - 1]}{" "}
+            </p>
+            <div className="buttons">
+              <button
+                onClick={handlePrevious}
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              >
+                Previous
+              </button>
+              <button
+                onClick={handleNext}
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              >
+                {" "}
+                Next{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>The page you are looking for is not here!</h1>
+          <button onClick={handleClopen} className='close'> &#10003; </button>
+        </div>
+      )}
     </div>
   );
 }
